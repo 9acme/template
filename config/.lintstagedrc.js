74f765341cea735ac40de925a87cdf725fda3740
周灿雄 <zhoucanxiong@xcxd.com>
2023-07-13 15:05:35 +0800

    init(所有): 第一次提交

---
+++ config/.lintstagedrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "src/**/*.js": [
+    "npx eslint -c config/.eslintrc.js --fix",
+    "prettier --config config/.prettierrc.js --write",
+  ],
+  "src/**/*.json": ["prettier --config config/.prettierrc.js --write"],
+  "src/**/*.(scss)": ["npx stylelint  --config config/.stylelintrc.js --fix"],
+  "src/**/*.html": ["prettier --config config/.prettierrc.js --write"],
+  "src/**/*.vue": [
+    "npx eslint -c config/.eslintrc.js --fix",
+    "npx stylelint --config config/.stylelintrc.js --fix",
+    "prettier --config config/.prettierrc.js --write",
+  ],
+};
---
+++ config/.release-it.js
@@ -0,0 +1,48 @@
+module.exports = {
+  github: {
+    release: false,
+  },
+  git: {
+    tagName: '${version}',
+    commitMessage: 'chore: v${version}',
+    branch: '202307_vueTemplate', // 推送到 202307_vueTemplate 分支
+  },
+  npm: {
+    publish: false,
+  },
+  hooks: {
+    'after:bump': 'echo 更新版本成功',
+  },
+  plugins: {
+    '@release-it/conventional-changelog': {
+      preset: {
+        name: 'conventionalcommits',
+        // 此处可定义需要哪些放至进 CHANGELOG.md 里，
+        // 默认只将 feat、fix写入日志
+        types: [
+          {
+            type: 'init',
+            section: '🎨初始化',
+          },
+          {
+            type: 'feat',
+            section: '✨新功能',
+          },
+          {
+            type: 'fix',
+            section: '🐞问题修复',
+          },
+          {
+            type: 'style',
+            section: '🌈样式',
+          },
+          {
+            type: 'docs',
+            section: '📃文档',
+          },
+        ],
+      },
+      infile: 'docs/CHANGELOG.md',
+    },
+  },
+};
